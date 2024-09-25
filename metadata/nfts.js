d44e7b1c2cc1ce6a4c2dda002630872eb4202f95
Gorisha2004 <157053342+Gorisha2004@users.noreply.github.com>
2024-09-25 22:07:22 +0530

    Create nfts.js

---
+++ metadata/nfts.js
@@ -0,0 +1,30 @@
+const nfts = [
+  {
+    name: "0",
+    description: "Music is an art.",
+    image: "QmdL362MF44qHLYVx4oUVvkSCpDNttSN3hTvPKUVm3aD8P",
+  },
+  {
+    name: "1",
+    description: "Music is an art.",
+    image: "QmYHUptBFLJbDjRBJxs3nsPtqYa3CSUNh3w8BeUTEMjFoQ",
+  }, //change
+  {
+    name: "2",
+    description: "Music is an art.",
+    image: "QmVYMcF5V8gKS8FeEC1xAe5rRpL6Q6kbjwGcGPpacUuNF5",
+  },
+  {
+    name: "3",
+    description: "Music is an art.",
+    image: "QmbTd8m7HctVJr9A4FL83ejcMmyuAXbU4FkL3QERmZGiLM",
+  },
+  {
+    name: "4",
+    description: "Music is an art..",
+    image: "QmRiB3kwnLP8BAudp6BNh2tfjKdwX4Hpmeigdrz45L9xAj",
+  },
+];
+
+module.exports = nfts;
+
